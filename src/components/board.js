c5c3b39fa84bfa6c71a17d0c3b5a11b1aaa3706f
ShivaKumar7799 <shivakumarbammidi@gmail.com>
2022-03-28 00:49:46 +0530

    constructor function updated

--- src/components/board.js
+++ src/components/board.js
@@ -1,195 +1,85 @@
 import { Button } from "./button";
-import {useState} from 'react';
-
+import {useEffect, useState} from 'react';
 
 function Board(){
-        let [count,setCount] = useState(1);
-        let [char0, setChar0] = useState("-");
-        let [char1, setChar1] = useState("-");
-        let [char2, setChar2] = useState("-");
-        let [char3, setChar3] = useState("-");
-        let [char4, setChar4] = useState("-");
-        let [char5, setChar5] = useState("-");
-        let [char6, setChar6] = useState("-");
-        let [char7, setChar7] = useState("-");
-        let [char8, setChar8] = useState("-");
-        let [gameWinner, setGameWinner] = useState("Game not started")
+        const [click,setClick] = useState(1)
+        const [count,setCount] = useState(1);
+        const [char0, setChar0] = useState("-");
+        const [char1, setChar1] = useState("-");
+        const [char2, setChar2] = useState("-");
+        const [char3, setChar3] = useState("-");
+        const [char4, setChar4] = useState("-");
+        const [char5, setChar5] = useState("-");
+        const [char6, setChar6] = useState("-");
+        const [char7, setChar7] = useState("-");
+        const [char8, setChar8] = useState("-");
+        const [gameWinner, setGameWinner] = useState("-")
         
+        let turn = function(){  
+               (count === 1) ? setCount(2) :setCount(1)
+        }
 
-
-        let turn = function(){
-                if(count === 1){
-                        setCount(2)
-                } else{
-                        setCount(1);
+        let PlayGame = function(char,setChar){
+                this.char = char;
+                this.setChar = setChar;
+                this.play =  function(){
+                        if(this.char === "-" && (gameWinner !==1 && gameWinner !==2) ){
+                                turn();
+                                if(count === 1){
+                                        this.setChar("X");
+                                } 
+                                else{
+                                        this.setChar("O");
+                                }
+                                winner();
+                        }                               
                 }
         }
-        
-        let playGame0 = function(){
-                if(char0 === "-" && (gameWinner !==1 && gameWinner !==2) ){
-                        turn();
-                        if(count === 1){
-                                char0 = "X"
-                                setChar0("X");
-                        } 
-                        else{
-                                char0 = "O"
-                                setChar0("O");
-                        }
-                        winner();
-                }              
-        }
 
-        let playGame1 = function(){
-                if(char1 === "-"  && (gameWinner !==1 && gameWinner !==2) ){
-                        turn();
-                        if(count === 1){
-                                char1 = "X"
-                                setChar1("X");
-                        } 
-                        else{
-                                char1 = "O"
-                                setChar1("O");
-                        }
-                        winner(); 
-                }             
-        }
+        let playGame0 = new PlayGame(char0, setChar0);
+        let playGame1 = new PlayGame(char1, setChar1);
+        let playGame2 = new PlayGame(char2, setChar2);
+        let playGame3 = new PlayGame(char3, setChar3);
+        let playGame4 = new PlayGame(char4, setChar4);
+        let playGame5 = new PlayGame(char5, setChar5);
+        let playGame6 = new PlayGame(char6, setChar6);
+        let playGame7 = new PlayGame(char7, setChar7); 
+        let playGame8 = new PlayGame(char8, setChar8);
 
-        let playGame2 = function(){
-                if(char2 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char2 = "X"
-                                setChar2("X");
-                        } 
-                        else{
-                                char2 = "O"
-                                setChar2("O");
-                        }
-                        winner(); 
-                }           
-        }
-
-        let playGame3 = function(){
-                if(char3 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char3 = "X"
-                                setChar3("X");
-                        } 
-                        else{
-                                char3 = "O"
-                                setChar3("O");
-                        }
-                        winner();   
-                } 
-                         
-        }
-
-        let playGame4 = function(){
-                if(char4 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char4 = "X"
-                                setChar4("X");
-                        } 
-                        else{
-                                char4 = "O"
-                                setChar4("O");
-                        }
-                        winner();   
-                }         
-        }
-
-        let playGame5 = function(){
-                if(char5 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char5 = "X"
-                                setChar5("X");
-                        } 
-                        else{
-                                char5 = "O"
-                                setChar5("O");
-                        }
-                        winner();  
-                }  
-                         
-        }
-
-        let playGame6 = function(){
-                if(char6 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char6 = "X"
-                                setChar6("X");
-                        } 
-                        else{
-                                char6 = "O"
-                                setChar6("O");
-                        }
-                        winner();  
-                } 
-                         
-        }
-
-        let playGame7 = function(){
-                if(char7 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char7 = "X"
-                                setChar7("X");
-                        } 
-                        else{
-                                char7 = "O"
-                                setChar7("O");
-                        }
-                        winner();  
-                }           
-        }
-
-        let playGame8 = function(){
-                if(char8 === "-"  && (gameWinner !==1 && gameWinner !==2)){
-                        turn();
-                        if(count === 1){
-                                char8 = "X"
-                                setChar8("X");
-                        } 
-                        else{
-                                char8 = "O"
-                                setChar8("O");
-                        }
-                        winner();  
-                }         
-        }
-
+       useEffect(()=>{
+               winner()
+                },[char0,char1,char2,char3,char4,char5,char6,char7,char8])
+      
         let winner = function(){
                 if((char0 === char1) && (char1 === char2) && (char2 !== "-") ){
-                        setGameWinner(count);
+                       count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char3 === char4) && (char4 === char5) && (char5 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char6 === char7) && (char7 === char8) && (char8 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char0 === char3) && (char3 === char6) && (char6 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char1 === char4) && (char4 === char7) && (char7 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char2 === char5) && (char5 === char8) && (char8 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char0 === char4) && (char4 === char8) && (char8 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
                 }
                 else if((char2 === char4) && (char4 === char6) && (char6 !== "-") ){
-                        setGameWinner(count);
+                        count === 1? setGameWinner(2) : setGameWinner(1)
+                }
+                else if(gameWinner === "-"){
+                       setGameWinner("Game is not yet started")
                 }
                 else{
-                       setGameWinner("Game is still going on")
+                        setGameWinner("Game is still going on")
                 }
         }
 
@@ -197,19 +87,19 @@ function Board(){
                 <>
                         <span> Player {count} turn </span>
                        <div>
-                               <Button text = {char0} callBack = {playGame0} />
-                               <Button text = {char1} callBack = {playGame1} />
-                               <Button text = {char2} callBack = {playGame2} />
+                               <Button text = {char0} callBack = {() => playGame0.play()} />
+                               <Button text = {char1} callBack = {() => playGame1.play()} />
+                               <Button text = {char2} callBack = {() => playGame2.play()} />
                        </div>
                        <div>
-                               <Button text = {char3} callBack = {playGame3} />
-                               <Button text = {char4} callBack = {playGame4} />
-                               <Button text = {char5} callBack = {playGame5} />
+                               <Button text = {char3} callBack =  {() => playGame3.play()} />
+                               <Button text = {char4} callBack =  {() => playGame4.play()} />
+                               <Button text = {char5} callBack =  {() => playGame5.play()} />
                        </div>
                        <div>
-                               <Button text = {char6} callBack = {playGame6} />
-                               <Button text = {char7} callBack = {playGame7} />
-                               <Button text = {char8} callBack = {playGame8} />
+                               <Button text = {char6} callBack =  {() => playGame6.play()} />
+                               <Button text = {char7} callBack =  {() => playGame7.play()} />
+                               <Button text = {char8} callBack =  {() => playGame8.play()} />
                        </div>
                        <span> Game Winner: Player {gameWinner} </span>
                 </>
